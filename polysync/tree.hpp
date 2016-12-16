38a5d3b76fceb0fb5a6652a9d55c194349704128
John Galbraith <jgalbraith@polysync.io>
2016-12-15 20:48:11 -0800

    Review node and tree data structures.
    Prior to this commit, the dynamic tree was working fine.  However, the code itself
    has never been reviewed.  This commit isolates the node structure for review.

--- polysync/tree.hpp
+++ polysync/tree.hpp
@@ -1,78 +1,79 @@
 #pragma once
 
-#include <polysync/exception.hpp>
-
-#include <eggs/variant.hpp>
 #include <vector>
 #include <algorithm>
 #include <iostream>
-#include <boost/optional.hpp>
-#include <boost/hana.hpp>
+
+#include <boost/multiprecision/cpp_int.hpp>
+
+#include <eggs/variant.hpp>
+
+#include <polysync/exception.hpp>
 
 // Dynamic parse trees are basically just vectors of "nodes".  A node knows
 // it's name and a strongly typed value.
 
-namespace polysync { 
-
-namespace hana = boost::hana;
+namespace polysync {
 
 // Ugh, eggs::variant lacks the recursive feature supported by the old
-// boost::variant, which makes it a PITA to implement a syntax tree (no nested
-// variants!).  The std::shared_ptr<tree> is a workaround for this eggs
+// boost::variant, which makes it a PITA to implement a parse tree (no nested
+// variants!).  The problem is that we cannot define std::vector<node> before
+// we have declared node (as node is an incomplete type unsupported by
+// std::vector).  The std::shared_ptr<tree> is a workaround for this eggs
 // limitation.  Apparently we are getting std::variant in C++17; maybe we can
 // factor out std::shared_ptr<tree> to just tree, then.
 
 struct node;
 
-struct tree : std::shared_ptr<std::vector<node>> {
-    std::string type;
+struct tree : std::shared_ptr< std::vector<node> > {
 
-    static tree create(const std::string& type) { 
-        tree res(type);
-        // res.type = type;
-        return res;
-    }
+    tree( const std::string& type ) :
+        std::shared_ptr< std::vector<node> >( new std::vector<node>() ),
+        type(type) {}
 
-    static tree create(const std::string& type, std::initializer_list<node> init) {
-        tree res(type, init);
-        // res.type = type;
-        return std::move(res);
-    }
+    // This initialization_list<> constructor should only be invoked within
+    // unit tests to describe test vectors.  The initialization ends up copy
+    // constructing each node which is too slow for performant applications.
+    tree( const std::string& type, std::initializer_list<node> init )
+        : std::shared_ptr< std::vector<node> >( new std::vector<node>( init ) ),
+          type(type) {}
 
-    tree() : std::shared_ptr<std::vector<node>>(new std::vector<node>()) {}
-    tree(const std::string& type) : std::shared_ptr<std::vector<node>>(new std::vector<node>()), type(type) {}
-    tree(const std::string& type, std::initializer_list<node> init) : std::shared_ptr<std::vector<node>>(new std::vector<node>(init)), type(type) {}
+    const std::string type;
 };
 
 // Add some context to exceptions
 namespace exception {
-using tree = boost::error_info<struct tree_type, tree>;
+    using tree = boost::error_info< struct tree_type, tree >;
 }
 
 // Fallback to a generic memory chunk when a description is unavailable.
 using bytes = std::vector<std::uint8_t>;
 
-// Each leaf node in the tree may contain any of a limited set of types, defined here.
+// Each node in the tree may contain any of a limited set of strong types.
 using variant = eggs::variant<
+
     // Nested types and vectors of nested types
     tree, std::vector<tree>,
 
-    // Undecoded raw bytes (fallback when description is missing)
-    bytes, 
+    // Undecoded raw bytes 
+    bytes,
 
     // Floating point types and native vectors
-    float, std::vector<float>, 
-    double, std::vector<double>, 
+    float, std::vector<float>,
+    double, std::vector<double>,
 
     // Integer types and native vectors
-    std::int8_t, std::vector<std::int8_t>, 
-    std::int16_t, std::vector<std::int16_t>, 
-    std::int32_t, std::vector<std::int32_t>, 
+    std::int8_t, std::vector<std::int8_t>,
+    std::int16_t, std::vector<std::int16_t>,
+    std::int32_t, std::vector<std::int32_t>,
     std::int64_t, std::vector<std::int64_t>,
-    std::uint8_t, 
-    std::uint16_t, std::vector<std::uint16_t>, 
-    std::uint32_t, std::vector<std::uint32_t>, 
-    std::uint64_t, std::vector<std::uint64_t>
+    std::uint8_t,
+    std::uint16_t, std::vector<std::uint16_t>,
+    std::uint32_t, std::vector<std::uint32_t>,
+    std::uint64_t, std::vector<std::uint64_t>,
+
+    // Integers longer than 64 bits
+    boost::multiprecision::cpp_int, std::vector<boost::multiprecision::cpp_int>
     >;
 
 // Dynamic parsing builds a tree of nodes to represent the record.  Each leaf
@@ -81,66 +82,48 @@ using variant = eggs::variant<
 struct node : variant {
 
     template <typename T>
-    node(const std::string& n, const T& value) : variant(value), name(n) { }
+    node( const std::string& n, const T& value ) : variant( value ), name( n ) { }
+
+    // Factory functions and decoders should return nodes by move for speed
+    node( node&& ) = default;
+
+    // The copy constructor should be used sparingly, because the move
+    // constructor is faster. Ideally, this constructor is invoked only in
+    // unit test vector construction.
+    node( const node& ) = default;
+
     const std::string name;
 
-    // Convert a hana structure into a vector of dynamic nodes.
-    template <typename Struct>
-    static node from(const Struct& s, const std::string&);
+    // nodes may have a specialized function for formatting the value to a string.
+    std::function<std::string ( const variant& )> format;
+
 };
 
-inline bool operator==(const tree& lhs, const tree& rhs) { 
-    if (lhs->size() != rhs->size())
+inline bool operator==( const tree& left, const tree& right ) {
+
+    if ( left->size() != right->size() or left.type != right.type )
         return false;
 
-    return std::equal(lhs->begin(), lhs->end(), rhs->begin(), rhs->end(), 
-            [](const node& ln, const node& rn) {
+    return std::equal( left->begin(), left->end(), right->begin(), right->end(),
+            []( const node& ln, const node& rn ) {
+
+	   	if ( ln.name != rn.name ) {
+		    return false;
+	        }
 
                 // If the two nodes are both trees, recurse.
                 const tree* ltree = ln.target<tree>();
                 const tree* rtree = rn.target<tree>();
-                if (ltree && rtree)
+                if (ltree && rtree) {
                     return operator==(*ltree, *rtree);
+	        }
 
                 // Otherwise, just use variant operator==()
                 return ln == rn;
             });
 }
-inline bool operator!=(const tree& lhs, const tree& rhs) { return !operator==(lhs, rhs); }
 
-// Convert a hana structure into a vector of dynamic nodes.
-template <typename Struct>
-inline node node::from(const Struct& s, const std::string& type) {
-    tree tr = tree::create(type);
-    hana::for_each(s, [tr](auto pair) { 
-            std::string name = hana::to<char const*>(hana::first(pair));
-            tr->emplace_back(name, hana::second(pair));
-            });
-        
-    return node(type, tr);
-}
-
-inline std::ostream& operator<<(std::ostream&, const tree&); 
+inline bool operator!=( const tree& lhs, const tree& rhs ) { return !operator==( lhs, rhs ); }
 
 } // namespace polysync
-
-#include <polysync/print.hpp>
-
-namespace polysync {
-
-inline std::ostream& operator<<(std::ostream& os, const node& n) {
-    if (n.target_type() == typeid(std::uint8_t))
-            return os << static_cast<std::uint16_t>(*n.target<std::uint8_t>());
-    if (n)
-        return eggs::variants::apply([&os](auto a) -> std::ostream& { return os << a; }, n);
-    return os << "unset";
-}
-
-
-inline std::ostream& operator<<(std::ostream& os, const tree& t) {
-    return os << *t;
-}
-
-} // polysync
-
 
