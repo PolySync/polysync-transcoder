a13c7b6c2d6851ff8ce5ae9088ffdf064e109e9e
John Galbraith <jgalbraith@polysync.io>
2016-12-15 16:20:56 -0800

    Add initial behavioral tests

---
+++ features/bad-inputs.feature
@@ -0,0 +1,25 @@
+Feature: Invalid parameters are handled gracefully
+    Scenario: No arguments provided
+        Given the command line: polysync-transcode
+        Then the return value is -2
+        And stdout is empty
+        And stderr contains: no input file
+
+    Scenario: Help is requested
+        Given the command line: polysync-transcode -h
+        Then the return value is 0
+        And stdout contains: Usage
+        And stderr is empty
+
+    Scenario: Invalid argument
+        Given the command line: polysync-transcode --verschwinden ibeo.25.plog 
+        Then the return value is -1
+        And stdout is empty
+        And stderr contains: unrecognised option
+
+    Scenario: Input file does not exist
+        Given the command line: polysync-transcode missing.plog dump
+        Then the return value is -2
+        And stdout is empty
+        And stderr contains: cannot open file
+
--- features/steps/transcode.py
+++ features/steps/transcode.py
@@ -1,36 +1,55 @@
 from behave import *
-from compare import expect
-import subprocess, shlex
 
-use_step_matcher("re")
-path = '../build/transcode'
+from hamcrest import *
+import subprocess, shlex, ctypes, os
 
-@given('the command line: (?P<cmdline>.+)')
+path = '../build/cmdline'
+
+@given('the command line: {cmdline}')
 def step_impl(context, cmdline):
+
+    # Turn off console coloring; it just interferes in the behavioral tests
     args = shlex.split(cmdline)
     args[0] = path + '/' + args[0] 
-    args.insert(1, '--plain')
-    print(args)
-    context.response = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # print (context.response)
+    args.insert(1, '--plain')  # turns of color escape codes
+    args.insert(1, '--plugdir=../build/plugin') # configure runtime environment
+    args.insert(1, '--plugdir=..')
+    if 'loglevel' in context.config.userdata:
+        args.insert(1, '--loglevel=' + context.config.userdata['loglevel'])
 
-@then('the return value is (?P<retval>.+)')
+    context.response = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+@then('the return value is {retval}')
 def step_impl(context, retval):
     # annoyingly, subprocess.run or the shell has made the exit status unsigned char
-    expect(context.response.returncode).to_equal(int(retval) & 0xFF)
+    assert_that(ctypes.c_int8(context.response.returncode).value, equal_to(int(retval)))
 
-@then('stdout contains: (?P<stdout>.*)')
+@then('stdout contains: {stdout}')
 def step_impl(context, stdout):
-    expect(context.response.stdout.decode('ascii')).to_contain(stdout)
+    assert_that(context.response.stdout.decode('ascii'), contains_string(stdout))
 
-@then('stderr contains: (?P<stderr>.*)')
+@then('stderr contains: {stderr}')
 def step_impl(context, stderr):
-    expect(context.response.stderr.decode('ascii')).to_contain(stderr)
+    assert_that(context.response.stderr.decode('ascii'), contains_string(stderr))
 
 @then('stdout is empty')
 def step_impl(context):
-    expect(not context.response.stdout)
+    assert_that(context.response.stdout, empty())
 
 @then('stderr is empty')
 def step_impl(context):
-    expect(not context.response.stderr)
+    assert_that(context.response.stderr, empty())
+
+@then('stdout does not contain: {stdout}')
+def step_impl(context, stdout):
+    assert_that(context.response.stdout.decode('ascii'), is_not(contains_string(stdout)))
+
+@then('the size of stdout is {size}')
+def step_impl(context, size):
+    assert_that(context.response.stdout, has_length(int(size)))
+
+@then('stdout is identical to the first {size} bytes of {path}')
+def step_impl(context, size, path):
+    binary_truth = open(path, 'rb').read(int(size))
+    assert_that(binary_truth, equal_to(context.response.stdout))
+
