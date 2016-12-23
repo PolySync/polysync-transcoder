24433bee5b8c401a3cdbee65805131e8b6dbf9d0
John Galbraith <jgalbraith@polysync.io>
2016-12-22 17:39:45 -0800

    Secure results partway through the refactor of decoder.

--- lib/exception.cpp
+++ lib/exception.cpp
@@ -1,32 +1,49 @@
-#include <polysync/exception.hpp>
 #include <polysync/console.hpp>
-#include <polysync/tree.hpp>
-#include <polysync/print_tree.hpp>
-#include <polysync/print_hana.hpp>
+#include <polysync/exception.hpp>
 #include <polysync/plog/core.hpp>
+#include <polysync/print_hana.hpp>
+#include <polysync/print_tree.hpp>
+#include <polysync/tree.hpp>
 
-namespace polysync {
+namespace polysync
+{
 
-std::ostream& operator<<(std::ostream& os, const error& e) {
+std::ostream& operator<<( std::ostream& os, const error& e )
+{
     os << e.what() << std::endl;
-    if (const std::string* module = boost::get_error_info<exception::module>(e))
-        os << "\tModule: " << format->fieldname(*module) << std::endl;
-    if (const plog::ps_log_record* record = boost::get_error_info<exception::ps_log_record>(e))
+    if ( const std::string* module = boost::get_error_info< exception::module >( e ) )
+    {
+        os << "\tModule: " << format->fieldname( *module ) << std::endl;
+    }
+    if ( const plog::ps_log_record* record = boost::get_error_info< exception::ps_log_record >( e ) )
+    {
         os << "\tRecord: " << *record << std::endl;
-    if (const std::string* tpname = boost::get_error_info<exception::type>(e))
-        os << "\tType: " << format->type(*tpname) << std::endl;
-    if (const std::string* fieldname = boost::get_error_info<exception::field>(e))
-        os << "\tField: " << format->fieldname(*fieldname) << std::endl;
-    if (const std::string* path = boost::get_error_info<exception::path>(e))
-        os << "\tPath: " << format->fieldname(*path) << std::endl;
-    if (const std::string* detector = boost::get_error_info<exception::detector>(e))
-        os << "\tDetector: " << format->fieldname(*detector) << std::endl;
-    if (const tree* tree = boost::get_error_info<exception::tree>(e))
+    }
+    if ( const std::string* tpname = boost::get_error_info< exception::type >( e ) )
+    {
+        os << "\tType: " << format->type( *tpname ) << std::endl;
+    }
+    if ( const std::string* fieldname = boost::get_error_info< exception::field >( e ) )
+    {
+        os << "\tField: " << format->fieldname( *fieldname ) << std::endl;
+    }
+    if ( const std::string* path = boost::get_error_info< exception::path >( e ) )
+    {
+        os << "\tPath: " << format->fieldname( *path ) << std::endl;
+    }
+    if ( const std::string* detector = boost::get_error_info< exception::detector >( e ) )
+    {
+        os << "\tDetector: " << format->fieldname( *detector ) << std::endl;
+    }
+    if ( const tree* tree = boost::get_error_info< exception::tree >( e ) )
+    {
         os << "\tPartial Decode: " << *tree << std::endl;
+    }
     return os;
 }
 
-std::string to_printable(const error& e) {
+std::string to_printable( const error& e )
+{
     std::stringstream os;
     os << e;
     return os.str();
--- polysync/plog/decoder.hpp
+++ polysync/plog/decoder.hpp
@@ -1,208 +1,174 @@
 #pragma once
 
+#include <fstream>
+
 #include <boost/endian/arithmetic.hpp>
 
 #include <polysync/tree.hpp>
 #include <polysync/descriptor.hpp>
-#include <polysync/size.hpp>
-#include <polysync/plog/core.hpp>
 #include <polysync/logging.hpp>
-#include <fstream>
+#include <polysync/plog/iterator.hpp>
 
 namespace polysync { namespace plog {
-
-namespace hana = boost::hana;
-using logging::severity;
-
-// Indirecting and incrementing an iterator requires reading the plog file
-// itself, because the offsets between records are always different, which we
-// cannot do until plog::decoder is defined.  So implement these functions
-// below, after decoder definition.  In the meantime, forward declare decoder
-// so we can prototype the methods.
-struct decoder;
-
-// Define an STL compatible iterator to traverse plog files
-struct iterator {
-
-    iterator(decoder*, std::streamoff);
-
-    decoder* stream;
-    std::streamoff pos; // file offset from std::ios_base::beg, pointing to next record
-    ps_log_record header;
-
-    bool operator!=(const iterator& other) const { return pos < other.pos; }
-    bool operator==(const iterator& other) const { return pos == other.pos; }
-
-    ps_log_record operator*(); // read and return the payload
-    iterator& operator++(); // skip to the next record
-};
 
 class decoder {
 public:
 
-    decoder(std::istream& st) : stream(st) {
-        stream.exceptions(std::ifstream::failbit);
+    decoder( std::istream& st );
 
-        // Find the last byte of the file
-        stream.seekg(0, std::ios_base::end);
-        endpos = stream.tellg();
-        stream.seekg(0, std::ios_base::beg);
-    }
+    // Construct STL compatible iterators
+    iterator begin();
+    iterator end();
 
-    // Factory methods for STL compatible iterators
-    iterator begin() { return iterator (this, stream.tellg()); }
-    iterator end() { return iterator (nullptr, endpos); }
-
-public:
     // Decode an entire record and return a parse tree.
-    variant deep(const ps_log_record&);
-    variant operator()(const descriptor::Type& type) { return decode(type); }
+    variant deep( const ps_log_record& );
+
+    variant operator()( const descriptor::Type& );
 
 public:
-    // Define a set of decode() templates, overloads, and specializations to pattern
-    // match every possible struct, sequence, or terminal type, nested or not,
-    // that is possibly found in a message.
+    // The decode() members are public only because the unit tests call them.
 
-    // For flat objects like arithmetic types, just straight copy memory as
-    // blob to file. In particular, most types that are not hana structures are these.
+    // Dynamic compound type from descriptor
+    variant decode( const descriptor::Type& );
+
+    // Dynamic compound type, but look up the description first by name
+    variant decode( const std::string& type );
+
+    // Factory functions of any supported type
+
+    template <typename T>
+    T decode( std::streamoff pos );
+
+    template <typename T>
+    T decode();
+
+    // Native arithmetic types (floats and integers)
     template <typename Number>
-    typename std::enable_if_t<std::is_arithmetic<Number>::value>
-    decode(Number& value) {
-        stream.read((char *)(&value), sizeof(Number));
-    }
+    typename std::enable_if_t< std::is_arithmetic<Number>::value >
+    decode( Number& );
+
+    // Integers longer than 64 bits (like ps_hash_type)
+    void decode( boost::multiprecision::cpp_int& );
 
     // Endian swapped types
-    template <typename T, size_t N>
-    void decode(boost::endian::endian_arithmetic<boost::endian::order::big, T, N>& value) {
-        stream.read((char *)value.data(), sizeof(T));
-    }
+    template < typename T, size_t N >
+    void decode( boost::endian::endian_arithmetic< boost::endian::order::big, T, N >& );
 
-    void decode(ps_hash_type& value) {
-        bytes buf(16); // 16 should be a template param, but I cannot get it to match.
-        stream.read((char *)buf.data(), buf.size());
-        multiprecision::import_bits(value, buf.begin(), buf.end(), 8);
-    }
+    // Hana exposed structures
+    template <typename S, class = typename std::enable_if_t< boost::hana::Struct<S>::value> >
+    void decode( S& );
 
-    // Specialize decode() for hana wrapped structures.  This works out padding
-    // in the structure definition where a straight memcpy would fail, and also
-    // recurses into nested structures.  Hana has a function hana::members()
-    // which would make this simpler, but sadly members() cannot return
-    // non-const references which we need here (we are setting the value).
-    template <typename S, class = typename std::enable_if_t<hana::Struct<S>::value>>
-    void decode(S& record) {
-        hana::for_each(hana::keys(record), [this, &record](auto&& key) mutable {
-                this->decode(hana::at_key(record, key));
-                });
-    }
-
-    // Sequences have a LenType number first specifying how many T's follow.  T
-    // might be a flat (arithmetic) type or a nested structure.  Either way,
-    // iterate the fields and serialize each one using the specialized decode().
+    // PolySync sequences (integer size, followed by that many bytes)
     template <typename LenType, typename T>
-    void decode(sequence<LenType, T>& seq) {
-        LenType len;
-        stream.read(reinterpret_cast<char *>(&len), sizeof(len));
-        seq.resize(len);
-        std::for_each(seq.begin(), seq.end(), [this](auto& val) mutable { this->decode(val); });
-    }
+    void decode( sequence<LenType, T>& );
 
-    // Specialize name_type because the underlying std::string type needs special
-    // handling.  It resembles a Pascal string (length first, no trailing zero
-    // as a C string would have)
-    void decode(ps_name_type& name) {
-        std::uint16_t len;
-        stream.read((char *)(&len), sizeof(len));
-        name.resize(len);
-        stream.read((char *)(name.data()), len);
-    }
+    // PolySync name type is specialized as std::string
+    void decode( ps_name_type& name );
 
-    void decode(bytes& raw) {
-        stream.read((char *)raw.data(), raw.size());
-    }
+    // Raw, uninterpreted bytes used when type description is unavailable
+    void decode( bytes& raw );
 
-    // Decode a dynamic type using the description name.
-    variant decode(const std::string& type);
+    // Decode any type, but seek to a known position first
+    template <typename T>
+    void decode( T&, std::streamoff );
 
-    variant decode(const descriptor::Type&);
+protected:
 
-    // Define a set of factory functions that know how to decode specific binary
-    // types.  They keys are strings from the "type" field of the TOML descriptions.
     using parser = std::function<variant (decoder&)>;
     static std::map<std::string, parser> parse_map;
 
-    // Factory function of any supported type, for convenience
-    template <typename T>
-    T decode() {
-        try {
-            T value;
-            decode(value);
-            return value;
-        } catch ( std::ios_base::failure ) {
-            throw polysync::error("read error")
-                << exception::module("plog::decoder");
-        }
-    }
-
-    // Deserialize a structure from a known offset in the file.  This is the
-    // method used when dereferencing the iterator, because the iterator knows
-    // the offset of a particular record.
-    template <typename T>
-    T decode(std::streamoff pos) {
-        stream.seekg(pos);
-        return decode<T>();
-    }
-
-    template <typename T>
-    void decode(T& value, std::streamoff pos) {
-        try
-        {
-            stream.seekg(pos);
-            decode<T>(value);
-        } catch ( std::ifstream::failure e)
-        {
-            throw polysync::error( "short read" );
-        }
-    }
-
-
-    // Expose the decode() members as operator(), for convenience.
-    template <typename... Args>
-    void operator()(Args... args) { decode(std::forward<Args>(args)...); }
-
-    logging::logger log { "decoder" };
-
-protected:
+    logging::logger log { "plog::decoder" };
 
     friend struct branch_builder;
     friend struct iterator;
 
     std::istream& stream;
 
-    std::streamoff endpos; // the last byte of the file
     std::streamoff record_endpos; // the last byte of the current record
 };
 
-inline iterator::iterator( decoder* s, std::streamoff pos ) : stream( s ), pos( pos ) {
-    if ( stream ) {
-        BOOST_LOG_SEV(stream->log, severity::debug1) << "decoding \"plog::ps_log_record\" at " << pos;
-        stream->decode( header, pos );
+template <typename Number>
+typename std::enable_if_t< std::is_arithmetic<Number>::value >
+decoder::decode( Number& value )
+{
+    stream.read( (char *)(&value), sizeof(Number) );
+}
+
+// Endian swapped types
+template <typename T, size_t N>
+void decoder::decode( boost::endian::endian_arithmetic<boost::endian::order::big, T, N>& value )
+{
+    stream.read( (char *)value.data(), sizeof(T) );
+}
+
+// Specialize decode() for hana wrapped structures.  This works out padding
+// in the structure definition where a straight memcpy would fail, and also
+// recurses into nested structures.  Hana has a function hana::members()
+// which would make this simpler, but sadly members() cannot return
+// non-const references which we need here (we are setting the value).
+template <typename S, class>
+void decoder::decode( S& record )
+{
+    namespace hana = boost::hana;
+    hana::for_each( hana::keys(record), [this, &record](auto&& key) mutable {
+            this->decode( hana::at_key(record, key) );
+            });
+}
+
+// Sequences have a LenType number first specifying how many T's follow.  T
+// might be a flat (arithmetic) type or a nested structure.  Either way,
+// iterate the fields and serialize each one using the specialized decode().
+template <typename LenType, typename T>
+void decoder::decode( sequence<LenType, T>& seq )
+{
+    LenType len;
+    stream.read( reinterpret_cast< char* >( &len ), sizeof( len ) );
+    seq.resize(len);
+    std::for_each(seq.begin(), seq.end(), [this]( auto& val ) mutable
+            { this->decode(val); });
+}
+
+  // Factory function of any supported type, for convenience
+template <typename T>
+T decoder::decode()
+{
+    try
+    {
+        T value;
+        decode(value);
+        return value;
+    }
+    catch ( std::ios_base::failure )
+    {
+        throw polysync::error("read error")
+            << exception::module("plog::decoder");
     }
 }
 
-inline ps_log_record iterator::operator*() {
-    return header;
+// Deserialize a structure from a known offset in the file.  This is the method
+// used when dereferencing the iterator, because the iterator knows the offset
+// of a particular record.  We have both a factory function version and set by
+// reference.
+template <typename T>
+void decoder::decode( T& value, std::streamoff pos )
+{
+    try
+    {
+        stream.seekg( pos );
+        decode<T>( value );
+    }
+    catch ( std::ifstream::failure )
+    {
+        throw polysync::error( "short read" );
+    }
 }
 
-inline iterator& iterator::operator++() {
-    static const std::streamoff recordsize = size<ps_log_record>::value();
-    // Advance the iterator's position to the beginning of the next record.
-    pos += recordsize + header.size;
-    if (pos < stream->endpos) {
-        BOOST_LOG_SEV(stream->log, severity::debug1)
-            << "decoding \"plog::ps_log_record\" at " << pos;
-        stream->decode(header, pos);
-    }
-    return *this;
+template <typename T>
+T decoder::decode( std::streamoff pos )
+{
+    stream.seekg(pos);
+    return decode<T>();
 }
+
 
 }} // namespace polysync::plog
