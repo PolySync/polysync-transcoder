18697d252480af6c1a204376e0dd0085653f43b7
John Galbraith <jgalbraith@polysync.io>
2017-01-11 15:59:03 -0800

    Port python3 subprocess stuff to python2
    Prior to this commit, there was still stuff in the behave suite that
    worked for python3 but not python2.

--- features/steps/transcode.py
+++ features/steps/transcode.py
@@ -9,13 +9,13 @@ def step_impl(context, cmdline):
     # Turn off console coloring; it just interferes in the behavioral tests
     args = shlex.split(cmdline)
     args.insert(1, '--plain')  # turns of color escape codes
-    args.insert(1, '--plugdir=../build/plugin') # configure runtime environment
-    args.insert(1, '--plugdir=..')
     if 'loglevel' in context.config.userdata:
         args.insert(1, '--loglevel=' + context.config.userdata['loglevel'])
 
     context.response = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     context.response.wait()
+    context.stdout = context.response.stdout.read().decode('ascii')
+    context.stderr = context.response.stderr.read().decode('ascii')
 
 @then('the return value is {retval}')
 def step_impl(context, retval):
@@ -24,30 +24,30 @@ def step_impl(context, retval):
 
 @then('stdout contains: {stdout}')
 def step_impl(context, stdout):
-    assert_that(context.response.stdout.read().decode('ascii'), contains_string(stdout))
+    assert_that(context.stdout, contains_string(stdout))
 
 @then('stderr contains: {stderr}')
 def step_impl(context, stderr):
-    assert_that(context.response.stderr.read().decode('ascii'), contains_string(stderr))
+    assert_that(context.stderr, contains_string(stderr))
 
 @then('stdout is empty')
 def step_impl(context):
-    assert_that(context.response.stdout.read(), empty())
+    assert_that(context.stdout, empty())
 
 @then('stderr is empty')
 def step_impl(context):
-    assert_that(context.response.stderr.read(), empty())
+    assert_that(context.stdout, empty())
 
 @then('stdout does not contain: {stdout}')
 def step_impl(context, stdout):
-    assert_that(context.response.stdout.decode('ascii'), is_not(contains_string(stdout)))
+    assert_that(context.stdout, is_not(contains_string(stdout)))
 
 @then('the size of stdout is {size}')
 def step_impl(context, size):
-    assert_that(context.response.stdout, has_length(int(size)))
+    assert_that(context.stdout, has_length(int(size)))
 
 @then('stdout is identical to the first {size} bytes of {path}')
 def step_impl(context, size, path):
     binary_truth = open(path, 'rb').read(int(size))
-    assert_that(binary_truth, equal_to(context.response.stdout))
+    assert_that(binary_truth, equal_to(context.stdout))
 
