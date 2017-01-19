59f570784bc0778931210648d035553823918a75
John Galbraith <jgalbraith@polysync.io>
2017-01-19 15:22:01 -0800

    Add a new field formatter to print both decimal and hex
    Prior to this commit, types could be dumped as either hex or decimal
    (the default).  For CAN, it is useful to print frame ID's as both
    because the various documents use both.  So now, there is a formatter
    called "decimal_and_hex".

--- lib/descriptor/formatter.cpp
+++ lib/descriptor/formatter.cpp
@@ -8,14 +8,23 @@
 namespace polysync { namespace descriptor {
 
 std::map< std::string, decltype( Field::format ) > formatFunction {
-    { "hex", [](const variant& n)
+    { "hex", []( const variant& value )
         {
             // The hex formatter just uses the default operator<<(), but kicks on std::hex first.
             std::stringstream os;
-            eggs::variants::apply([&os](auto v) { os << std::hex << "0x" << v << std::dec; }, n);
+            eggs::variants::apply( [ &os ]( auto v ) { os << std::hex << "0x" << v << std::dec; }, value );
             return os.str();
         }
     },
+    { "decimal_and_hex", []( const variant& value )
+        {
+            std::stringstream os;
+            eggs::variants::apply( [ &os ]( auto v ) {
+                    os << std::dec << v << " (" << std::hex << "0x" << v << ")" << std::dec;
+                    }, value );
+            return os.str();
+        }
+    }
         // Add additional formatters thusly:
     // { "NTP64", []( const variant& n )
     //     {
